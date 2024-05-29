6da236deb58e0d3cb7d98d5e29b13963342a699f
lags2022 <lguzman.58erb@outlook.com>
2024-05-28 19:47:06 -0500

    feat: terminando arquitectura limpia toktik

--- 5.prueba5/toktik/lib/presentation/providers/discover_provider.dart
+++ 5.prueba5/toktik/lib/presentation/providers/discover_provider.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
-import 'package:toktik/infrastructure/models/local_video_model.dart';
-import 'package:toktik/shared/data/local_video_posts.dart';
+import 'package:toktik/domain/repositories/video_post_repositorio.dart';
+// import 'package:toktik/infrastructure/models/local_video_model.dart';
+// import 'package:toktik/shared/data/local_video_posts.dart';
 
 class DiscoverProvider extends ChangeNotifier {
   // TODO: Repository, DataSource
+  final VideoPostRepository videosRepository;
 
   bool initialLoading = true;
   List<VideoPost> videos = [];
+
+  DiscoverProvider({required this.videosRepository});
 
   Future<void> loadNextPage() async {
     //todo: cargar videos
     // await Future.delayed(const Duration(seconds: 2));
 
-    final List<VideoPost> newVideos = videoPosts
-        .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
-        .toList();
+    // final List<VideoPost> newVideos = videoPosts
+    //     .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
+    //     .toList();
+
+    final newVideos = await videosRepository.getTrendingVideosByPage(1);
 
     videos.addAll(newVideos);
     initialLoading = false;
