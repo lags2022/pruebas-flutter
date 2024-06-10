3a86212afbbddb102c2b07970b000f58a4e1e449
Luis Angel <lguzman.58erb@outlook.com>
2024-06-10 09:45:59 -0500

    feat:añadiendo modelo yesnomodel fromjsonmap

---
+++ 0.pruebaborrar/pruebaborrar/lib/config/get_yes_no_answer.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:pruebaborrar/domain/entities/message.dart';
+import 'package:pruebaborrar/infrastructure/models/yes_no_model.dart';
+
+class GetYesNoAnswer {
+  final _dio = Dio();
+
+  Future<Message> getAnswer() async {
+    final response = await _dio.get("https://yesno.wtf/api");
+
+    final yesNoModel = YesNoModel.fromJsonMap(response.data);
+
+    return Message(
+        text: yesNoModel.answer,
+        fromWho: FromWho.hers,
+        imageUrl: yesNoModel.image);
+  }
+}
--- 0.pruebaborrar/pruebaborrar/lib/providers/chat_provider.dart
+++ 0.pruebaborrar/pruebaborrar/lib/providers/chat_provider.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:pruebaborrar/config/get_yes_no_answer.dart';
 import 'package:pruebaborrar/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
   final ScrollController chatScrollController = ScrollController();
+  final getYesNoAnswer = GetYesNoAnswer();
 
   List<Message> messageList = [
     Message(text: "fucking javascript", fromWho: FromWho.me),
@@ -15,6 +17,10 @@ class ChatProvider extends ChangeNotifier {
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messageList.add(newMessage);
 
+    if (text.endsWith("?")) {
+      herReply();
+    }
+
     notifyListeners();
     moveScrollToBottom();
   }
@@ -26,5 +32,13 @@ class ChatProvider extends ChangeNotifier {
         chatScrollController.position.maxScrollExtent,
         duration: const Duration(milliseconds: 300),
         curve: Curves.easeOut);
+  }
+
+  Future<void> herReply() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
+
+    messageList.add(herMessage);
+    notifyListeners();
+    moveScrollToBottom();
   }
 }
