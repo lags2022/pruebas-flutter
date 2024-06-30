4a9fd070f9ee83b050683d37f62f93a531fd0ff6
lags2022 <lguzman.58erb@outlook.com>
2024-06-29 21:29:52 -0500

    feat: frontend prueba go gin

--- 0.1toktik/toktik/lib/widgets/shared/video_buttons.dart
+++ 0.1toktik/toktik/lib/widgets/shared/video_buttons.dart
@@ -10,12 +10,38 @@ class VideoButtons extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
+        _CustomIconButton(
+            value: video.likes,
+            iconData: Icons.favorite,
+            iconColor: Colors.red),
+        _CustomIconButton(
+            value: video.likes, iconData: Icons.remove_red_eye_outlined),
+      ],
+    );
+  }
+}
+
+class _CustomIconButton extends StatelessWidget {
+  final int value;
+  final IconData iconData;
+  final Color? color;
+
+  const _CustomIconButton(
+      {required this.value, required this.iconData, iconColor})
+      : color = iconColor ?? Colors.white;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
         IconButton(
             onPressed: () {},
-            icon: const Icon(
-              Icons.favorite,
-              color: Colors.red,
-            ))
+            icon: Icon(
+              iconData,
+              color: color,
+              size: 30,
+            )),
+        Text("$value")
       ],
     );
   }
